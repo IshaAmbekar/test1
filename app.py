f8ed3e82f43d5bca67cdb5ac71a81c631aeefcbb
Isha Ambekar <isha.ambekar@cumminscollege.in>
2024-04-15 22:43:23 +0530

    added project

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import sqlite3
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__)
 
 # Function to initialize the database
 def initialize_database():
